6984f129f6463f6ea55723361276c6302005c94f
Miguel Vilao <miguel.rodrigues.vilao@atlantico.ao>
2023-02-01 10:20:24 +0100

    displyed chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/chat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/login_page.dart';
 
@@ -11,7 +12,8 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: ChatPage(),
     );
   }
 }
