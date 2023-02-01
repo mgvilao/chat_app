7ab5c88b9b0815db2b3ec05797c9cc4fb4470da9
Miguel Vilao <miguel.rodrigues.vilao@atlantico.ao>
2023-02-01 10:19:51 +0100

    playing around with basic layout widgets

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -10,7 +9,31 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat App'),
       ),
-      body: const Text('Hello again!'),
+      body: Column(
+        children: [
+          const Text(
+            'Let\'s sign you in!',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.brown,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const Text(
+            'Welcome back! \nYou\'ve been missed!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+          ),
+          Image.network(
+            'https://picsum.photos/250?image=9',
+            height: 200,
+          ),
+        ],
+      ),
     );
   }
 }
