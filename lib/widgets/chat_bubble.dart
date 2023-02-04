f2dff950cf68232a886c1f9bfa7e1a3ec183bc25
Miguel Vilao <miguel.rodrigues.vilao@atlantico.ao>
2023-02-04 11:37:55 +0100

    changed bubble style based on message sender

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -16,13 +16,19 @@ class ChatBubble extends StatelessWidget {
             BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.5),
         padding: const EdgeInsets.all(24),
         margin: const EdgeInsets.all(50),
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           color: Colors.grey,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(12),
-            topRight: Radius.circular(12),
-            bottomLeft: Radius.circular(12),
-          ),
+          borderRadius: message.sender.username == 'mgvilao'
+              ? const BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                )
+              : const BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
+                ),
         ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
